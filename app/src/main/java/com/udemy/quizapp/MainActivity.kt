740a9d26d319f927a16c5a7e826f4246b87de55c
김준기 <gugi329@daum.net>
2022-09-06 23:29:03 +0900

    CardView UI 세팅 및 상태 표시줄 비활성화

--- app/src/main/java/com/udemy/quizapp/MainActivity.kt
+++ app/src/main/java/com/udemy/quizapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.udemy.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udemy.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
 }
