f27ce38dcf59e9d0ff459930677ac29f00e474c9
김준기 <gugi329@daum.net>
2022-09-06 23:49:03 +0900

    Question UI 세팅 (2)

--- app/src/main/java/com/udemy/quizapp/QuestionActivity.kt
+++ app/src/main/java/com/udemy/quizapp/QuestionActivity.kt
@@ -19,6 +19,23 @@ class QuestionActivity : AppCompatActivity() {
     private fun getQuestionList() {
         val questionList = Constants.getQuestion()
         Log.i("question list size is", "${questionList.size}")
+
+        for (i in questionList) {
+            Log.e("Questions", i.question)
+        }
+
+        val currentPosition = 1
+        val question: Question = questionList[currentPosition - 1]
+
+        binding.ivFlag.setImageResource(question.image)
+        binding.progressBar.progress = currentPosition
+        binding.tvProgress.text =
+            resources.getString(R.string.tv_progress, currentPosition, binding.progressBar.max)
+        binding.tvQuestion.text = question.question
+        binding.tvFirstOption.text = question.firstOption
+        binding.tvSecondOption.text = question.secondOption
+        binding.tvThirdOption.text = question.thirdOption
+        binding.tvFourthOption.text = question.fourthOption
     }
 
 }
